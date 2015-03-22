693e14cc36e9df5d1f545fa89d97e9d5fb7e290b
Navdeep Saini <nav@collusionapp.com>
2015-03-22 14:20:36 +1300

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Navdeep Saini on 22/03/15.
+//  Copyright (c) 2015 Navdeep Saini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Navdeep Saini on 22/03/15.
+//  Copyright (c) 2015 Navdeep Saini. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
